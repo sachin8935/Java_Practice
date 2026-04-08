6dbf25fb618ab15f1ce2c9ffedd47dc6cb0eb269
Sachin Kumar <sachin8935@Sachins-MacBook-Air.local>
2026-04-08 11:58:00 +0530

    inventory management

--- basic_practise/src/MotorBikeRunner.java
+++
@@ -1,8 +0,0 @@
-public class MotorBikeRunner {
-    public static void main(String[] args){
-        Vehicle v1= new Bike();
-        Vehicle v2 = new MotorCar();
-        v1.start();
-        v2.start();
-    }
-}
